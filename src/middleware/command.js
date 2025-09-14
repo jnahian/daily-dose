ddbc646cbf56e06e6d72491786e9854fd6d05d2c
Julkar Naen Nahian <nahian048@gmail.com>
2025-09-14 12:17:15 +0600

    Commad logging added

--- src/middleware/command.js
+++ src/middleware/command.js
@@ -2,6 +2,7 @@ const { removeFormatting } = require("../utils/commandHelper");
 
 function stripFormatting() {
   return async ({ command, next }) => {
+    console.log("Command:", command);
     if (command.text) {
       // remove Slack-style formatting
       let clean = removeFormatting(command.text);
