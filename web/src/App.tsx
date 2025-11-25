78dd7e74a0dc9ba21003a71268c2ca6707ef362e
Julkar Naen Nahian <nahian048@gmail.com>
2025-11-25 13:45:48 +0600

    Fixed the duplicate router issue. The error should be resolved now - the BrowserRouter is only in main.tsx  where it should be, and App.tsx  just contains the Routes and Route components.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router'
 import Home from './pages/Home'
 import Docs from './pages/Docs'
 import Changelog from './pages/Changelog'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/docs" element={<Docs />} />
-        <Route path="/changelog" element={<Changelog />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/docs" element={<Docs />} />
+      <Route path="/changelog" element={<Changelog />} />
+    </Routes>
   )
 }
 
